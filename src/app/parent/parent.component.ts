f60f1ddfb6fa615d194a495875243bfc2c6c7f5b
hritikchokker <hchokker7@gmail.com>
2021-01-23 02:07:22 +0530

    2 files modified

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
+  listData =[{ id: 4, name: 'Laptop', rating: 3 },
+    { id: 5, name: 'Phone', rating: 4 },
+    { id: 6, name: 'Mice', rating: 4 }]
   constructor() { }
 
   ngOnInit() {
